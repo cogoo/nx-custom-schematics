24865eb770b67abd8cef3daf7901979e9f8b6cc7
Colin Ogoo <colin.ogoo@bynd.com>
2019-07-20 12:10:10 +0100

    feat: 🎸 run schematic

--- apps/adoption-ui/src/app/app.module.ts
+++ apps/adoption-ui/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { AuthAdoptionModule } from './auth-adoption/auth-adoption.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabled' })
+    RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+    AuthAdoptionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ apps/adoption-ui/src/app/auth-adoption/auth-adoption.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AuthAdoptionRoutingModule } from './auth-adoption-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AuthAdoptionRoutingModule
+  ]
+})
+export class AuthAdoptionModule { }
