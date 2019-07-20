f9ff92b8e3540378375e65706cda75559a39304e
Colin Ogoo <colin.ogoo@bynd.com>
2019-07-20 12:06:17 +0100

    feat: 🎸 write custom schematic

--- tools/schematics/auth-module/index.ts
+++ tools/schematics/auth-module/index.ts
@@ -1,9 +1,40 @@
 import { chain, externalSchematic, Rule } from '@angular-devkit/schematics';
+import * as path from 'path';
 
 export default function(schema: any): Rule {
+  if (!schema.name.startsWith('auth-')) {
+    throw new Error(`Auth modules must be prefixed with 'auth-'`);
+  }
   return chain([
-    externalSchematic('@nrwl/workspace', 'lib', {
-      name: schema.name
+    externalSchematic('@schematics/angular', 'module', {
+      project: schema.project,
+      name: schema.name,
+      routing: true,
+      module: 'app.module.ts'
+    }),
+    externalSchematic('@schematics/angular', 'service', {
+      project: schema.project,
+      name: schema.name,
+      path: path.join(
+        'apps',
+        schema.project,
+        'src',
+        'app',
+        schema.name,
+        'shared'
+      )
+    }),
+    externalSchematic('@schematics/angular', 'guard', {
+      project: schema.project,
+      name: schema.name,
+      path: path.join(
+        'apps',
+        schema.project,
+        'src',
+        'app',
+        schema.name,
+        'shared'
+      )
     })
   ]);
 }
