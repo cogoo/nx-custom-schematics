158b9c3d783f862e0df5955b9c4b402b71d1b679
Colin Ogoo <colin.ogoo@bynd.com>
2019-07-20 11:47:24 +0100

    feat: 🎸 add component to shared lib

--- libs/shared-components/src/lib/shared-components.module.ts
+++ libs/shared-components/src/lib/shared-components.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PetListComponent } from './pet-list/pet-list.component';
 
 @NgModule({
-  imports: [CommonModule]
+  imports: [CommonModule],
+  declarations: [PetListComponent],
+  exports: [PetListComponent]
 })
 export class SharedComponentsModule {}
